b7cf625eb37b5a942998ce2b2002b3815063af8f
Tiago Alves <tiago@softbuilder.con.br>
2022-02-24 19:30:37 -0300

    removendo # do link

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:tihrasguinho/src/app_module.dart';
 import 'package:tihrasguinho/src/app_widget.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() {
+  setPathUrlStrategy();
   runApp(ModularApp(module: AppModule(), child: const AppWidget()));
 }
